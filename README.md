056d6186faa11df6decbb6abe5949c29a54177b0
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-07-16 09:29:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# socfpga_kernel
+
+Linux kernel build cmake script for de0-nano-soc
+
+
---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+KERNELRELEASE=4.4.94
+target=socfpga
+cross_target=armhf
+target_device=de0-nano-soc
